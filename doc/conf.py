b5f03a035872d146940a770f55df70f23175dc22
salomaestro <chris10an.salomonsen@gmail.com>
2025-01-27 14:24:32 +0100

    Add sphinx documentation

---
+++ doc/conf.py
@@ -0,0 +1,16 @@
+project = "Christians' Project"
+copyright = "2025, Authors"
+author = "Christian Salomonsen"
+release = "0.1"
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+extensions = [
+    "myst_parser",  # in order to use markdown
+]
+
+myst_enable_extensions = [
+    "colon_fence",  # ::: can be used instead of ``` for better rendering
+]
+
+html_theme = "sphinx_rtd_theme"
